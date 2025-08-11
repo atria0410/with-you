1aed1b443b53848218788e62513d554305404bfa
DESKTOP-656DKLN\chika <atria0410@gmail.com>
2025-08-11 13:49:49 +0900

    Prettierを導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    rules: {
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'prefer-arrow-callback': 'error',
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  })
+]
 
-export default eslintConfig;
+export default eslintConfig
