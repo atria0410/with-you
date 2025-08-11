beff129dc6edf85a9e83600874edf0235706e271
DESKTOP-656DKLN\chika <atria0410@gmail.com>
2025-08-11 14:53:11 +0900

    背景コンポーネントを作成

---
+++ components/room.tsx
@@ -0,0 +1,39 @@
+import { tv } from 'tailwind-variants'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const roomVariants = tv({
+  base: 'flex h-screen items-center justify-center bg-cover bg-center',
+  variants: {
+    time: {
+      day: 'bg-[url(/images/room-day.jpg)]',
+      evening: 'bg-[url(/images/room-evening.jpg)]',
+      night: 'bg-[url(/images/room-night.jpg)]',
+      midnight: 'bg-[url(/images/room-midnight.jpg)]'
+    }
+  },
+  defaultVariants: {
+    time: 'day'
+  }
+})
+
+type Time = 'day' | 'evening' | 'night' | 'midnight' | undefined
+
+export const Room = ({ children }: Props) => {
+  const hour = new Date().getHours()
+  let time: Time
+
+  if (hour >= 6 && hour < 17) {
+    time = 'day'
+  } else if (hour >= 17 && hour < 19) {
+    time = 'evening'
+  } else if (hour >= 19 && hour < 23) {
+    time = 'night'
+  } else {
+    time = 'midnight'
+  }
+
+  return <div className={roomVariants({ time })}>{children}</div>
+}
